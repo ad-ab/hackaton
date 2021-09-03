9b199f5f960ef77db36c3c4c514a18be2eaa5129
Matej Strnad <matejstrnad@icloud.com>
2021-09-03 22:40:57 +0200

    Implement basic CREATE end-point.

--- api/webpages.js
+++ api/webpages.js
@@ -1,4 +1,6 @@
 const Router = require('koa-router');
+const { is, size, object, string, optional } = require('superstruct');
+const { v4: uuid } = require('uuid');
 
 module.exports = function webpages({ db }) {
   return new Router({ prefix: '/webPages/:location/comment' })
@@ -11,6 +13,42 @@ module.exports = function webpages({ db }) {
   }
 
   async function post(ctx) {
-    ctx.status = 200;
+    const CreatePostPayload = object({
+      author: size(string(), 1, 36),
+      text: size(string(), 5, 128000),
+      parent: optional(size(string(), 1, 36)),
+    });
+    if (!is(ctx.request.body, CreatePostPayload)) {
+      ctx.status = 400;
+      return;
+    }
+
+    const { author, text, parent: parentId } = ctx.request.body;
+
+    const collection = db.collection('comments');
+    const newComment = {
+      _id: uuid(),
+      author,
+      text,
+      created: Date.now(),
+      replies: [],
+    };
+
+    if (parentId) {
+      const parent = await collection.findOne({ _id: parentId });
+      if (!parent) {
+        ctx.status = 400;
+        return;
+      }
+
+      await collection.updateOne(
+        { _id: parentId },
+        { $push: { replies: newComment } }
+      );
+    } else {
+      await collection.insertOne(newComment);
+    }
+
+    ctx.status = 201;
   }
 };
