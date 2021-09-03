62a48330c9599471d8458e29d3bf76da19f5c7f5
Matej Strnad <matejstrnad@icloud.com>
2021-09-03 20:21:05 +0200

    Add logger.

--- api/index.js
+++ api/index.js
@@ -1,14 +1,10 @@
 const Koa = require('koa')
 const app = new Koa()
-const bodyParser = require('koa-bodyparser');
+const bodyParser = require('koa-bodyparser')
+const logger = require('./logger')
 
-app.use(bodyParser());
-
-app.use(async (ctx) => {
-
-  console.log(`${ctx.method} ${ctx.url} ${JSON.stringify(ctx.request.body.test,null, 2)}`)
-  ctx.body = 'OK'
-})
+app.use(bodyParser({ strict: false }))
+app.use(logger)
 
 app.listen(8000)
 console.log('Listening on 8000 ...')
---
+++ api/logger.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const csvStringify = require('csv-stringify/lib/sync')
+
+const FILE_PATH_TXT = './log.txt'
+const FILE_PATH_CSV = './log.csv'
+
+module.exports = async function logger(ctx) {
+  const time = new Date().toISOString()
+
+  fs.appendFileSync(FILE_PATH_TXT, `${time}: ${ctx.method} ${ctx.url}\n`)
+  fs.appendFileSync(
+    FILE_PATH_CSV,
+    csvStringify([
+      {
+        time,
+        method: ctx.method,
+        url: ctx.url,
+        body: ctx.request.body ? JSON.stringify(ctx.request.body) : '',
+      },
+    ])
+  )
+
+  console.log(`${ctx.method} ${ctx.url}`)
+}
