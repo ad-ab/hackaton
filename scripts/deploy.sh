aa6d9b972c44ae6cef88d44f454830d8d655dd75
Adam Abonyi <adam.abonyi@summitmedia.com>
2021-09-04 01:51:00 +0200

    Add server name to hosts in docker-compose

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,4 @@ docker save api:latest | sshpass -p hackathon ssh hackathon@10.6.32.113 "docker 
 docker image rm api:latest
 
 cat docker-compose.yml | sshpass -p hackathon ssh hackathon@10.6.32.113 "cat - > docker-compose.yml"
-sshpass -p hackathon ssh hackathon@10.6.32.113 "docker-compose down && docker-compose up"
+sshpass -p hackathon ssh hackathon@10.6.32.113 "docker-compose down && docker-compose up -d"
