90ee09cc21e44949c03a2bdbd8b418519b24f728
Matej Strnad <matejstrnad@icloud.com>
2021-09-04 11:33:17 +0200

    Disable validation logs in prod.

--- api/utils/validate.js
+++ api/utils/validate.js
@@ -3,7 +3,7 @@ const { validate } = require('superstruct');
 module.exports = (data, schema, ctx) => {
   const [error, result] = validate(data, schema);
 
-  if (!result) {
+  if (!result && process.env.NODE_ENV !== 'production') {
     console.error(`${ctx.method} ${ctx.url} ${error.message}`);
     return;
   }
